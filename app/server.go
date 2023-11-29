c6f77e6937ddf1804dae2637f9bb071a01490b98
h_kinjo <h.kinjo.11.8@gmail.com>
2023-11-29 23:31:33 +0900

    fix

--- app/server.go
+++ app/server.go
@@ -49,6 +49,6 @@ func main() {
 
   e.Logger.Fatal(e.Start(":8080"))
 }
-func Hello(c echo.Context) error {
-	return c.Render(http.StatusOK, "hello", "Worldaaaa")
-}
+// func Hello(c echo.Context) error {
+// 	return c.Render(http.StatusOK, "hello", "Worldaaaa")
+// }
