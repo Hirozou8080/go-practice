bbca304b3b7968b395444976c020b31205beb128
h_kinjo <h.kinjo.11.8@gmail.com>
2023-11-29 23:30:57 +0900

    別ファイル呼び出し

--- app/subserver.go
+++ app/subserver.go
@@ -1,14 +1,13 @@
 package main
 
-import (
-	"html/template"
-  "net/http"
-  "github.com/labstack/echo/v4"
-)
+// import (
+//   "net/http"
+//   "github.com/labstack/echo/v4"
+// )
 
-type CustomContext struct {
-	echo.Context
-}
-func Hello(c *CustomContext) {
-	return c.Render(http.StatusOK, "hello", "Worldaaaa")
-}
+// type CustomContext struct {
+// 	echo.Context
+// }
+// func Hello(c echo.Context) error {
+// 	return c.Render(http.StatusOK, "hello", "Worldaaaa")
+// }
