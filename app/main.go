635be3be8765c9406fb9514eaa85427fb9510f1b
Hirozou8080 <h.kinjo.11.8@gmail.com>
2023-12-15 17:01:16 +0900

    echo add

--- app/main.go
+++ app/main.go
@@ -1,11 +1,34 @@
 package main
 
-import "main/mypackage"
+import (
+	"net/http"
+	"main/mypackage"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-    // エクスポートされた関数を呼び出す
-    mypackage.ExportedFunction()
+  // インスタンスを作成
+	e := echo.New()
 
-    // 非エクスポートされた関数は他のファイルからは直接呼び出せない
-    // mypackage.nonExportedFunction()  // コンパイルエラーになります
+	// ミドルウェアを設定
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+  // ルートを設定
+  e.GET("/", hello) // ローカル環境の場合、http://localhost:1323/ にGETアクセスされるとhelloハンドラーを実行する
+
+  // サーバーをポート番号1323で起動
+  e.Logger.Fatal(e.Start(":8080"))
+
+  // エクスポートされた関数を呼び出す
+  mypackage.ExportedFunction()
+
+  // 非エクスポートされた関数は他のファイルからは直接呼び出せない
+  // mypackage.nonExportedFunction()  // コンパイルエラーになります
+}
+
+// ハンドラーを定義
+func hello(c echo.Context) error {
+  return c.String(http.StatusOK, "Hello, World!")
 }
